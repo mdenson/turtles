62a5067f7697ba2ecd0975a0a379038706443084
Brock Denson <mbdenson@gmail.com>
2017-05-02 18:38:23 -0700

    fix div w3-top element in menu

--- src/views/layout.js
+++ src/views/layout.js
@@ -3,7 +3,7 @@ var m = require("mithril")
 module.exports = {
 	view: function(vnode) {
 		return m("main.layout", [
-			m("w3-top"),
+			m(".w3-top", [
 				m("nav.w3-bar w3-red w3-card-2 w3-left-align w3-large", [
 					m("a.w3-bar-item w3-button w3-padding-large w3-white", { href: '/', oncreate: m.route.link }, "Home"),
 					m("a.w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red", { href: 'javascript:void(0);', onclick: "myFunction()", title: "Toggle Navigation Menu" }, "..." ),
@@ -13,7 +13,8 @@ module.exports = {
 				m("#navDemo.w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large", [
 					m("a.w3-bar-item w3-button w3-padding-large", { href: '/beaches', oncreate: m.route.link }, "Sites"),
 					m("a.w3-bar-item w3-button w3-padding-large", { href: '/surveys', oncreate: m.route.link }, "Surveys")
-				]),
+				])
+			]),
 			m("section", vnode.children),
 			m("footer.w3-container w3-padding-64 w3-center w3-opacity",
 				m(".w3-xlarge w3-padding-32", [
