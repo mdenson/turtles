f4d0d5230c5013cadc6378c3dd14c5b96e173af5
Brock Denson <mbdenson@gmail.com>
2018-05-01 12:15:16 -0700

    update beach list view

--- src/views/newbeachList.js
+++ src/views/newbeachList.js
@@ -12,16 +12,21 @@ module.exports = {
 				m("button.w3-button w3-padding w3-right", { href: "/beaches/edit/new", oncreate: m.route.link }, "New")
 			]),
 			// each beach - note '.id' is part of the result set, where '.doc.' is only there if the doc is included
-			m(".w3-container w3-padding", [
-				m("ul.w3-ul", [ Beach.list.map(function(b) {
-					console.log("beachList.view: Beach " + b.doc._id + " name " + b.doc.name)
-					return m("li", [
-						m("a.w3-button w3-right", { href: "/beaches/" + b.doc._id, oncreate: m.route.link }, "View"),
-						m("a.w3-button w3-right", { href: "/beaches/edit/" + b.doc._id, oncreate: m.route.link }, "Edit"),
-						m("span.w3-large", b.doc.name),m("br"),
-						m("span", b.doc.state + ", " + b.doc.country),
+			m(".w3-row-padding", [
+				Beach.list.map(function(b) {
+					return m(".w3-col m4 w3-padding-16", [
+						m(".w3-container w3-card-4 w3-dark-grey", [
+							m(".w3-container w3-center", [
+//								m("h1", "Pic"),
+								m("h1", b.doc.name),
+								m("h5", b.doc.state + ", " + b.doc.country)
+							],
+							m("a.w3-button w3-green", { href: "/beaches/" + b.doc._id, oncreate: m.route.link }, "View"),
+							m("a.w3-button w3-red", { href: "/beaches/edit/" + b.doc._id, oncreate: m.route.link }, "New Debris")
+							)
+						])
 					])
-				})]),
+				}),
 			]),
 			m(".w3-container w3-black w3-center", Beach.list.length + ((Beach.list.length == 1) ? " site" : " sites"))
 		])
